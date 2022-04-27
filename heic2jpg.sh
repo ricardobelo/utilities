115a267bb1d7f93fd9dd2e7fb9794276f99bfe66
Ricardo Belo <ricardobelo@users.noreply.github.com>
2022-04-27 20:42:45 -0300

    Update heic2jpg.sh

--- heic2jpg.sh
+++ heic2jpg.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #find . -type f -not -name "*.*" -exec bash -c 'if [ $(file --mime-type -b {}) == "image/heic" ]; then mv {} {}.heic; fi;' \;
 find . -type f -not -name "*.*" | while read FILE; do if [ $(file --mime-type -b "$FILE") == "image/heic" ]; then mv "$FILE" "$FILE".heic; fi; done;
-for i in *.heic; do heif-convert "$i" "${i%.heic}.jpg"; done
-rm -rf *.heic
+for i in *.HEIC; do heif-convert "$i" "${i%.heic}.jpg"; done
+mogrify -format jpg *.PNG
+rm -rf *.PNG *.HEIC
