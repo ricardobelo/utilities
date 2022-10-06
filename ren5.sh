951ce827b5b0fbe47db18ca959d8c4ecebdd642b
Ricardo Belo <ricardobelo@users.noreply.github.com>
2022-10-06 12:16:24 -0300

    Update ren5.sh

--- ren5.sh
+++ ren5.sh
@@ -1,3 +1,4 @@
-for FILE in *.jpg ; do mv "${FILE}" "${FILE:0:5}.jpg" ; done
+for FILE in *.*; do mv "${FILE}" "${FILE:0:5}.$(echo ${FILE##*.} | tr '[:upper:]' '[:lower:]')" ; done
+#for FILE in *.jpg ; do mv "${FILE}" "${FILE:0:5}.jpg" ; done
 #rename -n 's/(.{5}).*(\.jpg)$/$1$2/' *.jpg
 #IMG_[0-9]{4}.[a-Z]{3,4}
