63b8ffd4e6058227e757c0118697cd066df6222c
Ricardo Belo <ricardobelo@users.noreply.github.com>
2022-12-13 21:50:22 -0300

    Create update_image.sql

---
+++ update_image.sql
@@ -0,0 +1,3 @@
+#SELECT *  FROM `lc_product` WHERE `date_available` = '2022-12-12';
+#SELECT *  FROM `lc_product` WHERE product_id = 3021;
+#UPDATE lc_product SET image = CONCAT("catalog/tecidos/zibeline/", lc_product.sku, ".jpg") WHERE date_available = '2022-12-12' and model = 'zibeline';
