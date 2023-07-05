39498b87cfea6ffde884d06d0c84c45dc07ed16e
Ricardo Belo <ricardobelo@users.noreply.github.com>
2023-07-05 20:37:39 -0300

    Update ren5.sh

--- ren5.sh
+++ ren5.sh
@@ -1,4 +1,16 @@
-for FILE in *.*; do mv "${FILE}" "${FILE:0:5}.$(echo ${FILE##*.} | tr '[:upper:]' '[:lower:]')" ; done
+for file in *; do
+  newname=$(echo "$file" | tr -cd '[:digit:]_-')
+  newname=$(echo $newname | sed -e 's/[-_]*$//' -e 's/^[-_]*//')
+  if [ "$newname" != "$file" ] && [ -n "$newname" ] && [ ! -e "$newname" ]; then
+    if [ -e "$newname" ]; then
+      echo "O arquivo $newname já existe. Ignorando a renomeação do arquivo $file."
+    else
+      mv "$file" "$newname"
+    fi
+  fi
+done
+
+#for FILE in *.*; do mv "${FILE}" "${FILE:0:5}.$(echo ${FILE##*.} | tr '[:upper:]' '[:lower:]')" ; done
 #for FILE in *.jpg ; do mv "${FILE}" "${FILE:0:5}.jpg" ; done
 #rename -n 's/(.{5}).*(\.jpg)$/$1$2/' *.jpg
 #IMG_[0-9]{4}.[a-Z]{3,4}
