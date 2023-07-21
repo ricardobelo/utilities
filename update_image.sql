49626650515e1d5188450d154f99cd9429d8e5d0
Ricardo Belo <ricardobelo@users.noreply.github.com>
2023-07-21 16:39:51 -0300

    Update update_image.sql

--- update_image.sql
+++ update_image.sql
@@ -12,3 +12,19 @@
 #meta_description = replace(meta_description, 'Flamê', 'Flame'), 
 #meta_keyword = replace(meta_keyword, 'Flamê', 'Flame')
 #WHERE product_id IN (SELECT product_id FROM lc_product WHERE date_available = '2023-04-17' and model = 'cortina');
+
+
+
+
+#replace(meta_keyword, 'Flamê', 'Flame') 
+#CONCAT("prefix", lc_product_description.name, "sufix"),
+#SELECT * FROM lc_product_description WHERE product_id IN (SELECT product_id FROM lc_product WHERE date_available = '2023-07-21' and model = 'crepe_beethoven');
+
+UPDATE lc_product_description SET
+name = CONCAT("", lc_product_description.name, ""),
+description = CONCAT("", lc_product_description.description, ""),
+tag = CONCAT("", lc_product_description.tag, ""),
+meta_title = CONCAT("", lc_product_description.meta_title, ""),
+meta_description = CONCAT("", lc_product_description.meta_description, ""),
+meta_keyword = CONCAT("", lc_product_description.meta_keyword, ""),
+WHERE product_id IN (SELECT product_id FROM lc_product WHERE date_available = '2023-07-21' and model = 'crepe_beethoven');
