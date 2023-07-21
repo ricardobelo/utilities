f8a5b72e0b23f7acb392050749732b814b7912c4
Ricardo Belo <ricardobelo@users.noreply.github.com>
2023-07-21 16:55:19 -0300

    Update update_image.sql

--- update_image.sql
+++ update_image.sql
@@ -13,18 +13,17 @@
 #meta_keyword = replace(meta_keyword, 'Flamê', 'Flame')
 #WHERE product_id IN (SELECT product_id FROM lc_product WHERE date_available = '2023-04-17' and model = 'cortina');
 
-
-
-
+#########
 #replace(meta_keyword, 'Flamê', 'Flame') 
 #CONCAT("prefix", lc_product_description.name, "sufix"),
 #SELECT * FROM lc_product_description WHERE product_id IN (SELECT product_id FROM lc_product WHERE date_available = '2023-07-21' and model = 'crepe_beethoven');
+#SELECT * FROM lc_product_description WHERE product_id IN (SELECT product_id FROM lc_product WHERE date_available = '2023-07-21' and model = 'crepe_beethoven' and sku = codigo);
 
-UPDATE lc_product_description SET
-name = CONCAT("", lc_product_description.name, ""),
-description = CONCAT("", lc_product_description.description, ""),
-tag = CONCAT("", lc_product_description.tag, ""),
-meta_title = CONCAT("", lc_product_description.meta_title, ""),
-meta_description = CONCAT("", lc_product_description.meta_description, ""),
-meta_keyword = CONCAT("", lc_product_description.meta_keyword, ""),
-WHERE product_id IN (SELECT product_id FROM lc_product WHERE date_available = '2023-07-21' and model = 'crepe_beethoven');
+#UPDATE lc_product_description SET
+#name = CONCAT("", lc_product_description.name, "Preto"),
+#description = replace(meta_keyword, 'Crepe Beethoven', 'Crepe Beethoven Preto'),
+#tag = CONCAT("", lc_product_description.tag, "Preto"),
+#meta_title = CONCAT("", lc_product_description.meta_title, "Preto"),
+#meta_description = CONCAT("", lc_product_description.meta_description, "Preto"),
+#meta_keyword = CONCAT("", lc_product_description.meta_keyword, "Preto"),
+#WHERE product_id IN (SELECT product_id FROM lc_product WHERE date_available = '2023-07-21' and model = 'crepe_beethoven' and sku = 13976);
