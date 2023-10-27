7330fbc643f502aa5398ef83de1fd76f3f9fa287
Ricardo Belo <ricardobelo@users.noreply.github.com>
2023-10-27 01:19:21 -0300

    Update update_image.sql

--- update_image.sql
+++ update_image.sql
@@ -11,7 +11,7 @@
 #meta_title = replace(meta_title, 'Flamê', 'Flame'), 
 #meta_description = replace(meta_description, 'Flamê', 'Flame'), 
 #meta_keyword = replace(meta_keyword, 'Flamê', 'Flame')
-#WHERE product_id IN (SELECT product_id FROM lc_product WHERE date_available = '2023-04-17' and model = 'cortina');
+#WHERE product_id IN (SELECT product_id FROM lc_product WHERE date_available = '2023-04-17' and model = 'category_01');
 
 #########
 #replace(meta_keyword, 'Flamê', 'Flame') 
@@ -26,17 +26,23 @@
 #meta_title = CONCAT("", lc_product_description.meta_title, "Preto"),
 #meta_description = CONCAT("", lc_product_description.meta_description, "Preto"),
 #meta_keyword = CONCAT("", lc_product_description.meta_keyword, "Preto"),
-#WHERE product_id IN (SELECT product_id FROM lc_product WHERE date_available = '2023-07-21' and model = 'crepe_beethoven' and sku = 13976);
+#WHERE product_id IN (SELECT product_id FROM lc_product WHERE date_available = '2023-07-21' and model = 'category_01' and sku = 13976);
 
 #ADJUST CATEGORIES SUBCATEGORIES
 INSERT INTO lc_product_to_category (product_id, category_id)
-SELECT product_id, 99999 FROM lc_product WHERE date_available = '2023-07-27' and model = 'crepe_alfaiataria';
+SELECT product_id, 99999 FROM lc_product WHERE date_available = '2023-07-27' and model = 'category_01';
 
 #ADJUST CATEGORIES SUBCATEGORIES IF NOT EXIST
 INSERT INTO lc_product_to_category (product_id, category_id) 
 SELECT product_id, 99999 FROM lc_product WHERE model IN ('category_01', 'category_02') AND NOT EXISTS ( SELECT 1 FROM lc_product_to_category ptc WHERE ptc.product_id = lc_product.product_id AND ptc.category_id = 99999 );
 
+#SELECT
+SELECT lc_product.*, lc_product_to_category.*
+FROM lc_product
+JOIN lc_product_to_category ON lc_product_to_category.product_id = lc_product.product_id
+WHERE lc_product.model IN ('category_01', 'category_02') AND lc_product_to_category.category_id = 99999;
+
 #DELETE
 DELETE FROM lc_product_to_category
-WHERE product_id IN (SELECT product_id FROM lc_product WHERE model LIKE '%zibeline%') AND category_id = 525;
+WHERE product_id IN (SELECT product_id FROM lc_product WHERE model LIKE '%category_01%') AND category_id = 525;
 
