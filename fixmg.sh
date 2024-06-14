65392da1a494505a6beebd4501e1031b5cde40b7
Ricardo Belo <ricardobelo@users.noreply.github.com>
2024-06-14 20:19:31 -0300

    Update fixmg.sh

--- fixmg.sh
+++ fixmg.sh
@@ -14,3 +14,8 @@ find . -type f -exec sh -c 'mime_type=$(file --mime-type -b "$0"); case "$mime_t
 #for i in *.HEIC *.heic; do heif-convert "$i" "${i%.heic}.jpg"; done && for i in *.PNG *.png *.WEBP *.webp; do mogrify -format jpg "$i"; done
 #rm -rf *.PNG *.png *.HEIC *.heic *.WEBP *.webp 
 #mogrify -format jpg -sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace RGB *.jpg
+
+
+
+####### all in one
+ls -pr | grep -v / | sed -E 's/_[^_]*$//' | sed -E 's/(.*)\..*/\1/' | awk '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) tolower(substr($i,2)); print}' > ../$(basename $(pwd)).txt; find . -maxdepth 1 -type f -exec sh -c 'newname=$(echo "$0" | tr -cd "[:digit:]_-" | cut -c1-5); newname=$(echo "$newname" | sed -e "s/[-_]*$//" -e "s/^[-_]*//"); [ "$newname" != "$0" ] && [ -n "$newname" ] && [ ! -e "$newname" ] && mv "$0" "$newname"' {} \;; find . -type f -exec sh -c 'mime_type=$(file --mime-type -b "$0"); case "$mime_type" in "image/jpeg") new_extension=".jpg" ;; "image/heic") new_extension=".heic" ;; "image/png") new_extension=".png" ;; "image/webp") new_extension=".webp" ;; *) new_extension=".outro" ;; esac; filename=$(basename "$0"); extension="${filename##*.}"; [ "$extension" != "${new_extension#.}" ] && new_filename="${filename%.*}$new_extension" && mv "$0" "$(dirname "$0")/$new_filename"' {} \;
